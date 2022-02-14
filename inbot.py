6b26c2e739ceb67314d951782be8812b8110d145
yuraslivka <slivka.y@gmail.com>
2022-02-14 12:36:49 +0100

    140522

---
+++ inbot.py
@@ -0,0 +1,116 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from auth_data import username, password
+import time
+import random
+from selenium.common.exceptions import NoSuchElementException
+
+
+class InstaBot():
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.browser = webdriver.Chrome('./chromedriver/chromedriver')
+
+    # метод для закрытия браузера
+    def close_browser(self):
+
+        self.browser.close()
+        self.browser.quit()
+
+    # метод логина
+    def login(self):
+
+        browser = self.browser
+        browser.get('https://www.instagram.com')
+        time.sleep(random.randrange(3, 5))
+
+        username_input = browser.find_element_by_name('username')
+        username_input.clear()
+        username_input.send_keys(username)
+
+        time.sleep(2)
+
+        password_input = browser.find_element_by_name('password')
+        password_input.clear()
+        password_input.send_keys(password)
+
+        password_input.send_keys(Keys.ENTER)
+        time.sleep(10)
+
+    # метод ставит лайки по hashtag
+    def like_photo_by_hashtag(self, hashtag):
+
+        browser = self.browser
+        browser.get(f'https://www.instagram.com/explore/tags/{hashtag}/')
+        time.sleep(5)
+
+        for i in range(1, 4):
+            browser.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(random.randrange(3, 5))
+
+        hrefs = browser.find_elements_by_tag_name('a')
+        posts_urls = [item.get_attribute(
+            'href') for item in hrefs if "/p/" in item.get_attribute('href')]
+
+        for url in posts_urls:
+            try:
+                browser.get(url)
+                time.sleep(3)
+                like_button = browser.find_element_by_xpath(
+                    '/html/body/div[1]/section/main/div/div[1]/article/div[3]/section[1]/span[1]/button').click()
+                time.sleep(random.randrange(80, 100))
+            except Exception as ex:
+                print(ex)
+                self.close_browser()
+
+    # метод проверяет по xpath существует ли элемент на странице
+    def xpath_exists(self, url):
+
+        browser = self.browser
+        try:
+            browser.find_element_by_xpath(url)
+            exist = True
+        except NoSuchElementException:
+            exist = False
+        return exist
+
+    # метод проверяет по xpath существует ли элемент на странице
+    def xpath_exists(self, url):
+
+        browser = self.browser
+        try:
+            browser.find_element_by_xpath(url)
+            exist = True
+        except NoSuchElementException:
+            exist = False
+        return exist
+
+    # метод ставит лайк на пост по прямой ссылке
+    def put_exactly_like(self, userpost):
+
+        browser = self.browser
+        browser.get(userpost)
+        time.sleep(4)
+
+        wrong_userpage = "/html/body/div[1]/section/main/div/h2"
+        if self.xpath_exists(wrong_userpage):
+            print("Такого поста не существует, проверьте URL")
+            self.close_browser()
+        else:
+            print("Пост успешно найден, ставим лайк!")
+            time.sleep(2)
+
+            like_button = browser.find_element_by_class_name(
+                'fr66n').click()
+            time.sleep(2)
+
+            print(f"Лайк на пост: {userpost} поставлен!")
+            self.close_browser()
+
+
+my_bot = InstaBot(username, password)
+my_bot.login()
+my_bot.put_exactly_like("https://www.instagram.com/p/CZmazdTMBAF/")
